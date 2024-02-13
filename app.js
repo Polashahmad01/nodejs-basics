299dea6bdf7b61d3a769d77bc3840b09eeacef2e
Polashahmad01 <dev.polashahmad@gmail.com>
2024-02-13 07:21:18 +0600

    Add form handling and a POST route for adding a product

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();
+app.use(express.urlencoded({ extended: true }));
+
+// app.use((req, res, next) => {
+//   console.log('In the middleware');
+//   next();
+// });
+
+// app.use('/', (req, res, next) => {
+//   console.log('This always runs');
+//   next();
+// });
+
+app.use('/add-product', (req, res, next) => {
+  // console.log('In the another middleware');
+  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-  console.log('In the second middleware');
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  // console.log('In the another middleware');
   res.send('<h1>Hello from Express.js</h1>');
 });
 
