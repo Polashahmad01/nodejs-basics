1a54b97f0b40fbe0b307bbb18a0ade2f5556413a
Polashahmad01 <dev.polashahmad@gmail.com>
2024-02-15 00:22:38 +0600

    Add Product functionality and Product model with file storage

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,17 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', activeAddProduct: true, formCSS: true, productCSS: true });
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', { prods: products, docTitle: 'Shop', path: '/', pageTitle: 'Shop', hasProducts: products.length > 0, acitveShop: true, productCSS: true });
+  Product.fetchAll((products) => {
+    res.render('shop', { prods: products, docTitle: 'Shop', path: '/', pageTitle: 'Shop', hasProducts: products.length > 0, acitveShop: true, productCSS: true });
+  });
 }
