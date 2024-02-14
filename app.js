8a09e242414099ea06c1f50d8645757b85ee9d1c
Polashahmad01 <dev.polashahmad@gmail.com>
2024-02-14 13:20:07 +0600

    Refactor app.js and add ejs view files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const rootDir = require('./util/path')
 
 const express = require('express');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
@@ -14,10 +14,14 @@ const shopRoutes = require('./routes/shop');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Express Handlebars
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+//Ejs
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+//Express Handlebars
+// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
 
 //Pug
 // app.set('view engine', 'pug');
@@ -37,7 +41,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render('page-not-found', { pageTitle: 'Page Not Found'});
+  res.render('page-not-found', { pageTitle: 'Page Not Found', path: '' });
 });
 
 app.listen(3000);
