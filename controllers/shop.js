98fd0ed72abb92c164a42d364b77b2d5ebede9ab
Polashahmad01 <dev.polashahmad@gmail.com>
2024-02-16 07:54:00 +0600

    Add getOrders route and view

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,6 +29,14 @@ exports.getCart = (req, res, next) => {
   });
 }
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Yours orders'
+  });
+}
+
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
