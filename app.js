49712544afd42b0127e2cab7663949ec92871056
Polashahmad01 <dev.polashahmad@gmail.com>
2024-02-20 22:52:06 +0600

    Refactor error handling and cart deletion functionality

--- app.js
+++ app.js
@@ -1,47 +1,24 @@
 const path = require('path');
 
-const rootDir = require('./util/path')
-
 const express = require('express');
-// const expressHbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
-const errorsController = require('./controllers/error');
+const errorController = require('./controllers/error');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-
-const shopRoutes = require('./routes/shop');
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//Express Handlebars
-// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
-// app.set('view engine', 'hbs');
-// app.set('views', 'views');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-//Pug
-// app.set('view engine', 'pug');
-// app.set('views',  'views');
-
-// app.use((req, res, next) => {
-//   console.log('In the middleware');
-//   next();
-// });
-
-// app.use('/', (req, res, next) => {
-//   console.log('This always runs');
-//   next();
-// });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorsController.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,14 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      docTitle: 'All Products',
-      path: '/products',
-      pageTitle: 'Shop'
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
-}
+};
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -20,46 +19,66 @@ exports.getProduct = (req, res, next) => {
       pageTitle: product.title,
       path: '/products'
     });
-  })
-}
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
-      docTitle: 'Shop',
-      path: '/',
-      pageTitle: 'Shop'
+      pageTitle: 'Shop',
+      path: '/'
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
-}
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
-  })
+  });
   res.redirect('/cart');
-}
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Yours orders'
+    pageTitle: 'Your Orders'
   });
-}
-
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
   });
-}
+};
